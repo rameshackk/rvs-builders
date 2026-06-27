ac332cac96df5161e17b8497b176f236dcf886c8
Ramesh <rameshpec28@gmail.com>
2026-06-27 16:39:59 +0530

    Use relative base path for asset compatibility

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [react()],
-    base: command === 'serve' ? '/' : '/rvs-builders/',
-  }
+export default defineConfig({
+  plugins: [react()],
+  base: "./",
 })
