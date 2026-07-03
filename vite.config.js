b8021a41857716fa03fd264cb789efad53f3e868
Ramesh <rameshpec28@gmail.com>
2026-07-03 19:55:09 +0530

    Fix: use hash filenames to bypass CDN cache

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/rvs-builders/',
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/index.js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]',
-      }
-    }
-  }
 })
